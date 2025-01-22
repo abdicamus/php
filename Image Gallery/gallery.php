3f71cdaab0bdebe341f28f4979a6483d261b7da5
Ans <te.ais@hotmail.com>
2025-01-22 17:02:30 +0500

    Разработан проект Image Gallery

--- Image Gallery/gallery.php
+++ Image Gallery/gallery.php
@@ -5,5 +5,34 @@ include './inc/images.inc.php';
 ?>
 <?php include './views/header.php'; ?>
 
-
 <?php include './views/footer.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Форма с карточками</title>
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+
+<body>
+    <div class="container">
+        <?php foreach ($imageTitles as $key => $value): ?>
+            <a href="image.php?image=<?php echo e($key); ?>">
+                <div class="card">
+                    <img src="images/<?php echo e($key); ?>" alt="<?php echo e($key); ?>">
+                    <p><?php echo e($value) ?></p>
+                </div>
+            </a>
+
+            <?php if (isset($_GET[$key])): ?>
+                <?php $clickedLink = $_GET[$key]; ?>
+                <?php header("Location: $clickedLink"); ?>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    </div>
+</body>
+
+</html>
--- Image Gallery/image.php
+++ Image Gallery/image.php
@@ -7,3 +7,11 @@ include './inc/images.inc.php';
 
 
 <?php include './views/footer.php'; ?>
+
+<?php foreach ($imageDescriptions as $key => $value): ?>
+    <?php if (isset($_GET['image']) && $key == $_GET['image']): ?>
+        <img src="images/<?php echo e($key); ?>" alt="<?php echo e($key); ?>">
+        <p> <?php echo e($value); ?> </p>
+    <?php endif; ?>
+<?php endforeach; ?>
+<a href="">Go back</a>
